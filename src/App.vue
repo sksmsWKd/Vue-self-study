52ecddc9025d6938bd19f46c5a507f1b96997a9d
Song Jae Hyeon <0nocsace@gmail.com>
2021-08-09 19:36:47 +0900

    Vue add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
     <Header></Header>
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <div id="content" class="content">
+      <router-view></router-view>
+    </div>
+    Parent counter : {{ parentCounter }}<br />
+
+    <!-- Child 컴포넌트를 등록하고 counter 데이터 속성을 props로 전달한다. -->
+    <child></child>
+
+    <br />
+    <br />
+
+    <div id="app">
+      <button @click="$store.commit('increment')">증가</button>
+      <button @click="$store.commit('decrement')">감소</button>
+      <button @click="$store.commit('doubleadd', 5)">버튼</button>
+      {{ count }}
+
+      <h1>{{ $store.getters.counting }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/layout/Header.vue";
 
+// App.vue(Parent)
+import Child from "./Child.vue";
+
 export default {
   name: "App",
   components: {
+    // Child 컴포넌트를 하위 컴포넌트로 등록
+    child: Child,
     Header,
+  },
+  // data() {
+  //   return {
+  //     counter: 0,
+  //   };
+  // },
+  methods: {
+    // 이벤트 추가
+    addCounter() {
+      this.$store.commit("addCounter");
+    },
+    subCounter() {
+      this.$store.state.counter--;
+    },
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
   },
 };
 </script>
