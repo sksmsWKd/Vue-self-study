23c10a5a937ca42ff8c1c6a223f77cdb4787baba
Song Jae Hyeon <0nocsace@gmail.com>
2021-08-10 15:36:14 +0900

    Vue add router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import {
+  store
+} from "./store"
+
 import {
   BootstrapVue,
-  IconsPlugin
+  IconsPlugin,
+
 } from 'bootstrap-vue'
+
+
 
 Vue.config.productionTip = false
 
@@ -16,5 +24,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
+  router,
+  store: store,
   render: h => h(App),
 }).$mount('#app')
