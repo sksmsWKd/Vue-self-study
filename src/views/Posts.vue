d8b147cbc899289e4c9fb905179268b36275e5a1
Song Jae Hyeon <0nocsace@gmail.com>
2021-08-25 13:32:22 +0900

    Vue post

---
+++ src/views/Posts.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h1>Posts</h1>
+
+    <v-flex xs6>
+      <Postlist></Postlist>
+    </v-flex>
+  </v-container>
+</template>
+<script>
+import Postlist from "../components/Posts/Postlist.vue";
+export default {
+  components: {
+    Postlist,
+  },
+};
+</script>
