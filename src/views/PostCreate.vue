b66e2265cd8d198b2309545569148458d50a1f46
Song Jae Hyeon <0nocsace@gmail.com>
2021-10-10 20:31:27 +0900

    modal test

--- src/views/PostCreate.vue
+++ src/views/PostCreate.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <v-container>
+
+    <!-- <v-container>
       <v-flex xs6>
         <h1>ㅎ_ㅇ</h1>
         <v-text-field label="제목" v-model="title"> </v-text-field>
@@ -12,8 +12,98 @@
 
       <h3>이미지예시</h3>
       https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Tree_Sparrow_August_2007_Osaka_Japan.jpg/250px-Tree_Sparrow_August_2007_Osaka_Japan.jpg
-    </v-container>
-  </div>
+    </v-container> -->
+ <div class="container-fluid mt-5 justify-content-center">
+    <div class="row">
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">asddddddd456546</div>
+          <div class="card-body">zxc</div>
+        </div>
+      </div>
+      <div class="w-100"></div>
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">zz567asdasdasdasd</div>
+          <div class="card-body">456asd</div>
+        </div>
+      </div>
+    </div>
+    <div class="w-100"></div>
+    <div class="row">
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">
+            <input type="text" placeholder="여기 입력" v-model="string" />
+          </div>
+          <div class="card-body">
+            <p>{{ string }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-100"></div>
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">aasadsdasd</div>
+          <div class="card-body">
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-toggle="modal"
+              data-target="#exampleModal"
+            >
+              Launch demo modal
+            </button>
+
+            <!-- Modal -->
+            <div
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                      Modal title
+                    </h5>
+                    <button
+                      type="button"
+                      class="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">...</div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <button
+                      @click="goToEvents"
+                      type="button"
+                      class="btn btn-primary"
+                    >
+                      Save changes
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+ 
 </template>
 <script>
 import { mapMutations } from "vuex";
